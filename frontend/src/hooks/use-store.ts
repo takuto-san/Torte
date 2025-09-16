5bf2f1d9f68d834448218dffac729ece34b12fb6
takuto-abc <tmtydoraemon2@gmail.com>
2025-09-16 14:49:42 +0900

    fix: TypeError

--- frontend/src/hooks/use-store.ts
+++ frontend/src/hooks/use-store.ts
@@ -1,6 +1,6 @@
 "use client"
 import { useDispatch, useSelector, useStore } from 'react-redux'
-import type { RootState, AppDispatch, AppStore } from '@/lib/store'
+import type { RootState, AppDispatch, AppStore } from '@/lib/stores/store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
